d83b682b55bd4a29e410df4ce70d3e7bf02d74d3
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-07-11 16:19:01 -0700

    hello world

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'normalize.css';
+
+const component = 'Hello World';
+
+ReactDOM.render(
+  component,
+  document.getElementById('root')
+);
