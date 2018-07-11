a12e1c4785cf83ae097c9a1c75bc4446e5bdd890
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-07-11 16:54:05 -0700

    added an image short bio and links

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+import portrait from '../../media/portrait.png';
 
 export default class App extends Component {
   render() {
     return (
       <div>
-        Hello World!
+        <img src={portrait} style={{ width: '100vw' }}/>
+        <h1>Hello, I'm Stephanie.</h1>
+        <a href="https://github.com/stephaniesmith">GitHub</a>
+        <a href="https://www.linkedin.com/in/stephanie-lauren-smith/">LinkedIn</a>
       </div>
     );
   }
