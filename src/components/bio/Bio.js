437e7ef09ff68d29d64a4ca39bec70f8c90dcaac
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-07-12 15:10:12 -0700

    bio and image

--- src/components/bio/Bio.js
+++ src/components/bio/Bio.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import styles from './Bio.css';
+import stephanie from '../../media/Stephanie.jpg';
 
 export default class Bio extends Component {
   
@@ -17,10 +18,9 @@ export default class Bio extends Component {
     return (
       <div className={styles.bio}>
         <button onClick={() => this.handleClass('')}>home</button>
-        <p>Full Stack-JavaScript developer with a degree in industrial
-design. I enjoy collaborating, learning new skills, and analyzing
-code to find solutions. As an avid hiker and creative, you can
-find me in the mountains making videos about the PNW.</p>
+        <img src={stephanie}/>
+        <h1>A little about me.</h1>
+        <p>As a Full Stack-JavaScript developer with a background in industrial design, I enjoy collaborating, learning new skills, and analyzing code to find solutions. Currently I'm writing JavaScript using a MERN stack and styling it up with SCSS, but excited to learn new languages, frameworks, and libraries! I do both 2d and 3d design work, predominantly using the Adobe Suite and SolidWorks. Outside of tech, I'm an avid hiker and creative, you can find me in the mountains making videos about the PNW.</p>
       </div>
     );
   }
