b202c2b43ce23643c2262e2e26a0b34e32cb2180
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-07-12 12:10:07 -0700

    created bio component

---
+++ src/components/bio/Bio.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class Bio extends Component {
+  render() {
+    return (
+      <div>
+        <p>This is a bio.</p>
+      </div>
+    );
+  }
+}
