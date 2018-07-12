187e075b36ea80536dd29da5eba0dc7ebd1652c9
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-07-11 17:56:44 -0700

    basic css

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
 import portrait from '../../media/portrait.png';
+import styles from './App.css';
 
 export default class App extends Component {
   render() {
     return (
-      <div>
-        <img src={portrait} style={{ width: '100vw' }}/>
+      <div className={styles.app}>
         <h1>Hello, I'm Stephanie.</h1>
         <a href="https://github.com/stephaniesmith">GitHub</a>
         <a href="https://www.linkedin.com/in/stephanie-lauren-smith/">LinkedIn</a>
