0ba75744499acf83b74c41529e49bfef00992532
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-07-12 13:10:54 -0700

    working on the changing classes on click

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 import Home from '../home/Home';
+import Bio from '../bio/Bio';
+import styles from './App.css'
 
 export default class App extends Component {
   render() {
     return (
-      <div>
-        <Home/>
+      <div className={styles.app}>
+        <div className="wrapper">
+          <Home/>
+          <Bio/>
+        </div>
       </div>
     );
   }
--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -2,12 +2,28 @@ import React, { Component } from 'react';
 import styles from './Home.css';
 
 export default class Home extends Component {
+
+  state = {
+    active: ''
+  };
+
+  bioTransition = () => {
+    this.setState({
+      active: 'bio'
+    });
+  };
+
   render() {
+    const { active } = this.state;
+
+    const bioClass = active ? `${styles.home} ${active}` : styles.home;
+
     return (
-      <div className={styles.home}>
+      <div className={bioClass}>
         <h1>Hello, I'm Stephanie.</h1>
         <a href="https://github.com/stephaniesmith">GitHub</a>
         <a href="https://www.linkedin.com/in/stephanie-lauren-smith/">LinkedIn</a>
+        <button onClick={this.bioTransition}>bio</button>
       </div>
     );
   }
