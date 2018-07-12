a0f460f734db5f8b271a5305c4ff7af881fcee9a
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-07-12 14:22:11 -0700

    bio stuf

--- src/components/bio/Bio.js
+++ src/components/bio/Bio.js
@@ -17,7 +17,10 @@ export default class Bio extends Component {
     return (
       <div className={styles.bio}>
         <button onClick={() => this.handleClass('')}>home</button>
-        <p>This is a bio.</p>
+        <p>Full Stack-JavaScript developer with a degree in industrial
+design. I enjoy collaborating, learning new skills, and analyzing
+code to find solutions. As an avid hiker and creative, you can
+find me in the mountains making videos about the PNW.</p>
       </div>
     );
   }
