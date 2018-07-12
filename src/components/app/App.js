ad9da9dd3c438f02310d2397c35d9b9f4b356100
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-07-12 12:08:09 -0700

    created home component

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,14 +1,11 @@
 import React, { Component } from 'react';
-import portrait from '../../media/portrait.png';
-import styles from './App.css';
+import Home from './Home';
 
 export default class App extends Component {
   render() {
     return (
-      <div className={styles.app}>
-        <h1>Hello, I'm Stephanie.</h1>
-        <a href="https://github.com/stephaniesmith">GitHub</a>
-        <a href="https://www.linkedin.com/in/stephanie-lauren-smith/">LinkedIn</a>
+      <div>
+        <Home/>
       </div>
     );
   }
---
+++ src/components/app/Home.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import portrait from '../../media/portrait.png';
+import styles from './Home.css';
+
+export default class Home extends Component {
+  render() {
+    return (
+      <div className={styles.home}>
+        <h1>Hello, I'm Stephanie.</h1>
+        <a href="https://github.com/stephaniesmith">GitHub</a>
+        <a href="https://www.linkedin.com/in/stephanie-lauren-smith/">LinkedIn</a>
+      </div>
+    );
+  }
+}
